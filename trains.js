0d76ef208b6cde4fa8d933efc51286fd6112c0a9
Prabhashi Wijesinghe <85472209+Prabhashi-97@users.noreply.github.com>
2025-12-17 13:32:18 +0530

    Create trains.js

---
+++ trains.js
@@ -0,0 +1,31 @@
+
+[
+  {"Code": "BA 341",
+   "Origin": "London",
+   "DueTime": "2025-01-10T15:00:00Z",
+    "Status": {
+      "arrived": "14:00",
+      "expected_at": "14:00",
+      "cancelled": false
+    }
+  },
+  {"Code": "BA 342",
+   "Origin": "London",
+   "DueTime": "2025-01-10T15:00:00Z",
+   "Status": {
+      "arrived": "",
+      "expected_at": "14:00",
+      "cancelled": true
+    }
+  },
+  {"Code": "BA 343",
+    "Origin": "London",
+    "DueTime": "2025-01-10T15:00:00Z",
+    "Status": {
+        "arrived":null,
+        "expected_at": "15:00",
+        "cancelled": false
+    }
+  }
+
+]
